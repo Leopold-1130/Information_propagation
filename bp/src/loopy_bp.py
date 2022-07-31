59148fcc799dbe294ba070801ba166ffd1e0cb60
Johan Kok <johan.kok@grabtaxi.com>
2022-07-31 20:39:02 +0800

    Implemented validation loss for loopy BP

--- bp/src/loopy_bp.py
+++ bp/src/loopy_bp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import torch
+import torch.nn as nn
 from torch_geometric.datasets import Planetoid
 from utils.GraphConverter import GraphConverter
 
@@ -9,12 +10,26 @@ if __name__ == "__main__":
 	edge_H 		= torch.load(os.path.join(os.getcwd(), "matrices", "pubmed", "H.pt"))
 	edge_H 		= edge_H.detach().numpy()
 
+	observed_nodes 		= torch.where(plaintoid_g.val_mask == False)[0].tolist()
+	observed_node_class = plaintoid_g.y[plaintoid_g.val_mask == False].tolist()
+
 	factor_g 	= GraphConverter.planetoid_to_factor_graph(	plaintoid_g 		= plaintoid_g, 
 															num_classes 		= plaintoid_g.y.max() +1,
 															edge_H 				= edge_H,
-															observed_nodes 		= [14, 2],
-															observed_node_class = [0, 1]
+															observed_nodes 		= observed_nodes,
+															observed_node_class = observed_node_class
 														)
 
 	iters, converged = factor_g.lbp(normalize = True, max_iters = 100)
 	print(f"Iters: {iters}, Converged {converged}")
+
+	marginals 				= factor_g.rv_marginals()
+	unobserved_nodes 		= torch.where(plaintoid_g.val_mask == True)[0].tolist()
+	
+	unobserved_node_class 	= plaintoid_g.y[plaintoid_g.val_mask == True]
+	predicted_class 		= filter(lambda x: int(x[0].name) in unobserved_nodes, marginals)
+	predicted_class 		= list(map(lambda x: x[-1], predicted_class))
+	predicted_class 		= torch.tensor(predicted_class)
+	loss_fnct 				= nn.CrossEntropyLoss(reduction = "mean")
+	loss = loss_fnct(predicted_class, unobserved_node_class)
+	print(loss)
